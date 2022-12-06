0533ffe10b5e3fec97f5ebb82631be235a626e05
alfarai <73865991+alfarai@users.noreply.github.com>
2022-12-06 12:06:46 +0800

    Updated credits and add pdf
    - file chooser functionality implemented
    - added pictures in credits

--- app/src/main/java/com/example/wan2readdigitallibrary/add.java
+++ app/src/main/java/com/example/wan2readdigitallibrary/add.java
@@ -2,13 +2,43 @@ package com.example.wan2readdigitallibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.view.View;
+import android.widget.Button;
 
 public class add extends AppCompatActivity {
+    // Request code for selecting a PDF document.
+    private static final int PICK_PDF_FILE = 2;
 
+    private void openFile(Uri pickerInitialUri) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/pdf");
+
+        // Optionally, specify a URI for the file that should appear in the
+        // system file picker when it loads.
+        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
+
+        startActivityForResult(intent, PICK_PDF_FILE);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        Button uploadBtn = findViewById(R.id.uploadbtn);
+
+        uploadBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                openFile(null);
+            }
+        });
+
+
+
+
     }
 }
